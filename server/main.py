37c522918bdaac49e0e9dc76d5aa65a81f61c8eb
kamaukimani <paulkimys285@gmail.com>
2026-09-23 15:45:53 +0300

    set up flask application to run

---
+++ server/main.py
@@ -0,0 +1,10 @@
+from app import create_app 
+
+app=create_app()
+
+@app.route('/')
+def index():
+    return "Index for Plant RESTful API"
+
+if __name__=="__main__":
+    app.run(port=5555,debug=True)
