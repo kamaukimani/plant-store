fb662fcdcd131594e71fd0a533a0cbd9a4f53728
kamaukimani <paulkimys285@gmail.com>
2026-09-23 16:48:29 +0300

    add Plants GET method

---
+++ server/app/routes/plant.py
@@ -0,0 +1,12 @@
+from flask_restful import Resource 
+from flask import make_response,request 
+from app.models import Plant
+
+class Plants(Resource):
+    def get(self):
+        plants=[plant.to_dict() for plant in Plant.query.all()]
+        response=make_response(
+            plants,
+            200
+        )
+        return response
