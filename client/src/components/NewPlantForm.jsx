10f0e7683fd94b5896314b07d86aec1881ebfea9
kamaukimani <paulkimys285@gmail.com>
2026-09-23 17:38:20 +0300

    clear input fields after the form submits

--- client/src/components/NewPlantForm.jsx
+++ client/src/components/NewPlantForm.jsx
@@ -19,7 +19,12 @@ function NewPlantForm({ onAddPlant }) {
       }),
     })
       .then((r) => r.json())
-      .then((newPlant) => onAddPlant(newPlant));
+      .then((newPlant) => {
+        onAddPlant(newPlant)
+        setName("")
+        setImage("")
+        setPrice("")
+      });
   }
 
   return (
