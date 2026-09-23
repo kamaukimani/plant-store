883359057b78c2cc69b74a608c619e0ccdda6715
kamaukimani <paulkimys285@gmail.com>
2026-09-23 16:08:29 +0300

    add code to populate database

---
+++ server/seed.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from app import create_app
+from app.models import Plant
+from app.db import db
+
+app=create_app()
+with app.app_context():
+
+    Plant.query.delete()
+
+    aloe = Plant(
+        id=1,
+        name="Aloe",
+        image="/images/aloe.jpg",
+        price=11.50,
+    )
+
+    zz_plant = Plant(
+        id=2,
+        name="ZZ Plant",
+        image="/images/zz-plant.jpg",
+        price=25.98,
+    )
+    calathea = Plant(
+        id=2,
+        name="Calathea",
+        image="/images/calathea.jpg",
+        price=30.00,
+    )
+    jade = Plant(
+        id=2,
+        name="Jade",
+        image="/images/jade.jpg",
+        price=22.67,
+    )
+    fiddle = Plant(
+        id=2,
+        name="Fiddle-leaf",
+        image="/images/fiddle-leaf.jpg",
+        price=40.67,
+    )
+    monstera = Plant(
+        id=2,
+        name="Monstera ",
+        image="/images/monstera.jpg",
+        price=25.98,
+    )
+    pilea = Plant(
+        id=2,
+        name="Pilea",
+        image="/images/pilea.jpg",
+        price=25.98,
+    )
+    pothos = Plant(
+        id=2,
+        name="Pothos",
+        image="/images/pothos.jpg",
+        price=25.98,
+    )
+
+    plants=[aloe,calathea,fiddle,jade,monstera,pilea,pothos,zz_plant]
+    db.session.add_all(plants)
+    db.session.commit()
