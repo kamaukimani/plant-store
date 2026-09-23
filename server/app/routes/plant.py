dae4ba0daad2a9bc92dd24c4dd0fb8c0f0f27c12
kamaukimani <paulkimys285@gmail.com>
2026-09-23 17:27:16 +0300

    add PATCH method to be able to edit plants name,image and price

--- server/app/routes/plant.py
+++ server/app/routes/plant.py
@@ -11,4 +11,57 @@ class Plants(Resource):
             200
         )
         return response
+    def post(self):
+        data=request.get_json()
+
+        plant=Plant()
+        allowed_fields=["name","image","price"]
+
+        for attr in data:
+            if attr in allowed_fields:
+                setattr(plant,attr,data[attr])
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict=plant.to_dict()
+
+        response=make_response(
+            plant_dict,
+            201
+        )
+        return response
+
+class PlantsById(Resource):
+    def get(self,id):
+        plant=Plant.query.filter(Plant.id == id).first()
+        if plant is None:
+            return {
+                "message":"OOOOpppss!!!The plant does not exist in our database"
+            },404
+        plant_dict=plant.to_dict()
+        response=make_response(
+            plant_dict,
+            200
+        )
+        return response
+    def patch(self,id):
+        plant=Plant.query.filter_by(id=id).first()
+        if plant is None:
+            return {
+                "message":"OOOOpppss!!!The plant does not exist in our database"
+            },404
+        data=request.get_json()
+        allowed_fields=["name","price"]
+
+        for attr,value in data.items():
+            if attr in allowed_fields:
+                setattr(plant,attr,value)
+        db.session.commit()
+        plant_dict=plant.to_dict()
+        response=make_response(
+            plant_dict,
+            200
+        )
+        return response
     
+
